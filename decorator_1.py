bfcf55f51a61522df77fef35856aa9f02da61146
ivqing <2404021163@qq.com>
2015-01-18 18:44:22 +0800

    decorator with bug

---
+++ decorator_1.py
@@ -0,0 +1,15 @@
+import functools
+
+# decorator, print the log of func
+def log(func):
+    @functools.wraps(func) # decorator handling signature problem
+    def wrapper(*args, **kw):
+        print('begin call %s' % func.__name__)
+        return func(*args, **kw)
+    return wrapper
+
+@log
+def nothing():
+   print('hello decorator') 
+
+nothing()
